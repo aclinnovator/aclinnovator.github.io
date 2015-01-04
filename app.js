767c29c764d9ebb858067613c948565a6bb775de
aclinnovator <akivalipshitz@Akivas-Air-2.fios-router.home>
2015-01-04 01:55:54 -0500

    V1.0.1

---
+++ app.js
@@ -0,0 +1,18 @@
+// $(window).scroll(function(){
+// 	console.log("Window loaded");
+// 		if ($(this).scrollTop() >10) {
+// 				console.log("Scroll event hit")
+// 		} else {
+// 			$('#building').fadeOut(300);
+// 		}		
+// });
+
+$(document).ready(function(){
+	console.log("Document loaded");
+
+	$('#building').slideToggle(300);
+
+	$('.pillar').click(function(){
+
+	});
+});
